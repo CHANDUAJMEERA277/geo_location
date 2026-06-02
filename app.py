4fc3bb76c70be6433fcace7e117a0a22f1bb9318
CHANDUAJMEERA277 <chanduajmeera277@gmail.com>
2026-06-02 11:46:02 +0530

    render deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from datetime import datetime
 import json
+import os
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 
-    # REAL PUBLIC IP THROUGH NGROK
+    # GET REAL PUBLIC IP
     ip = request.headers.get(
         'X-Forwarded-For',
         request.remote_addr
@@ -231,5 +232,5 @@ def location():
 # -------------------------------------------------
 app.run(
     host='0.0.0.0',
-    port=5000
+    port=int(os.environ.get("PORT", 5000))
 )
